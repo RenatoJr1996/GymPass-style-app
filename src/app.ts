16aca78548e563bd0d82dbf9831cacd400d613f1
Renato Correa Junior <rcorrea@bluelab.com.br>
2024-05-20 14:51:22 -0300

    feat: Add prismaORM and build

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
 
 export const app = fastify()
 
-
+export const prisma = new PrismaClient()
